afd29d305147109a70daf135b0447dec97eb7b47
Angie <arco527@gmail.com>
2018-11-09 20:25:42 -0500

    Asociations

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,23 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @author = Author.find_by(name:"Angie")
+    @book = @author.books.create(book_params)
+    redirect_to books_path
+  end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title)
+  end
+end
